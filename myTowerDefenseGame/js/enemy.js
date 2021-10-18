dfce45c24525ab6e9fe18bab1dec19f9e8bfe06d
Ke Ren <ren00040@algonquinlive.com>
2021-10-17 23:23:55 -0400

    draw a sprite(enemy) animation

--- myTowerDefenseGame/js/enemy.js
+++ myTowerDefenseGame/js/enemy.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2021-10-14 23:12:30
  * @LastEditors: Ke Ren
- * @LastEditTime: 2021-10-15 13:42:13
+ * @LastEditTime: 2021-10-17 23:21:50
  * @FilePath: /myTowerDefenseGame/js/enemy.js
  */
 
@@ -54,36 +54,53 @@ function createPathway(){
 // draw the path way
 createPathway();
 
-
-const enemiesContainer = document.createElement("div");
-enemiesContainer.setAttribute("id","enemies");
-enemiesContainer.setAttribute("class","enemies");
-document.querySelector("body").append(enemiesContainer);
-
-function createEnemies() {
-    const enemyDiv = document.createElement("div");
-    enemyDiv.setAttribute("id","enemy");
-    enemyDiv.setAttribute("class","enemy");
-
-    const enemy = new Enemy("death",5,"./resource/enemies/death_scythe.png",100);
-    
-    const enemyContent = `
-        <img src = "${enemy.url}">
-    `;
-
-    // enemy height and width
-    const height = 33;
-    const width = 20;
-    let arr = [1,2,3,0]; // 0-up; 1-down 2-left 3-right
-
-    enemiesContainer.append(enemyDiv);
-    enemyDiv.innerHTML = enemyContent;
-
-    // enemyDiv.style.backgroundPositionY = ar
+let enemyImg = new Image();
+enemyImg.src = "./resource/enemies/fox.png";
+enemyImg.onload = function () {
+    drawEnemies();
 }
 
-createEnemies();
+// draw enemies
+let enemyWidth = 32;
+let enemyHeight = 32;
+function drawEnemies() {
+    console.log("draw enemies");
+    window.requestAnimationFrame(animationStep);
 
-function enemyMove() {
-    createEnemies();
+}
+
+function drawEnemyFrame(frameX, frameY, canvasX, canvasY) {
+    battleCTX.drawImage(enemyImg,
+                        frameX * enemyWidth, frameY * enemyHeight, enemyWidth, enemyHeight,
+                        canvasX, canvasY, enemyWidth, enemyHeight);
+
+}
+
+/* 
+ * Draw a sprite animation
+ * Reference: Animating Sprite Sheets With JavaScript (Martin Himmel)
+ * Link: https://dev.to/martyhimmel/animating-sprite-sheets-with-javascript-ag3
+ */
+const walkLoop = [0,1,0,2];
+let currentLoopIndex = 0;
+let frameCount = 0;
+
+function animationStep() {
+    frameCount++;
+    if(frameCount<15) {
+        window.requestAnimationFrame(animationStep);
+        return;
+    }
+
+    frameCount = 0;
+    
+    // refresh the canvas
+    battleCTX.clearRect(0,0,mapCanvas.width, mapCanvas.height);
+    drawEnemyFrame(walkLoop[currentLoopIndex++],0,0,0);
+    if(currentLoopIndex >= walkLoop.length) {
+        currentLoopIndex = 0;
+    }
+
+    // update the sprite's animation
+    window.requestAnimationFrame(animationStep);
 }
